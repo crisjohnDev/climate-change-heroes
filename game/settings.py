60b0e24f963fe5219be9603d29b563e02431eb60
crisjohn.dev.official@gmail.com <crisjohn.dev.official@gmail.com>
2026-09-08 11:23:18 +0800

    ad

--- game/settings.py
+++ game/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'student'
+    'student',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -135,3 +137,4 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+CORS_ALLOW_ALL_ORIGINS = True
